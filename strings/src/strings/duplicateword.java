fcc655b15e841720988f816489ae2ff90ef5a848
Lenovo <Lenovo@LAPTOP-92OO24AI>
2020-05-13 18:50:54 +0530

    string program

---
+++ strings/src/strings/duplicateword.java
@@ -0,0 +1,31 @@
+package strings;
+
+public class duplicateword {
+	public static void main(String[] args) {    
+        String string = "Dog is black and duck is black dog";    
+        int count;    
+            
+        //Converts the string into lowercase    
+        string = string.toLowerCase();    
+            
+        //Split the string into words using built-in function    
+        String words[] = string.split(" ");    
+            
+        System.out.println("Duplicate words in a given string : ");     
+        for(int i = 0; i < words.length; i++) {    
+            count = 1;    
+            for(int j = i+1; j < words.length; j++) {    
+                if(words[i].equals(words[j])) {    
+                    count++;    
+                    //Set words[j] to 0 to avoid printing visited word    
+                    words[j] = "0";    
+                }    
+            }    
+                
+            //Displays the duplicate word if count is greater than 1    
+            if(count > 1 && words[i] != "0")    
+                System.out.println(words[i]);    
+        }    
+    }    
+
+}
