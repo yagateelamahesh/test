f45c50d0ca4c8778add4d0ad2695650dfb04f3f2
Lenovo <Lenovo@LAPTOP-92OO24AI>
2020-05-18 20:32:50 +0530

    encapsulate

---
+++ oops/src/oops/encapsulation.java
@@ -0,0 +1,45 @@
+package oops;
+
+public class encapsulation
+{ 
+    
+    private String geekName; 
+    private int geekRoll; 
+    private int geekAge; 
+  
+    
+    public int getAge()  
+    { 
+      return geekAge; 
+    } 
+   
+    
+    public String getName()  
+    { 
+      return geekName; 
+    } 
+      
+   
+    public int getRoll()  
+    { 
+       return geekRoll; 
+    } 
+   
+    
+    public void setAge( int newAge) 
+    { 
+      geekAge = newAge; 
+    } 
+   
+    
+    public void setName(String newName) 
+    { 
+      geekName = newName; 
+    } 
+      
+   
+    public void setRoll( int newRoll)  
+    { 
+      geekRoll = newRoll; 
+    } 
+} 
